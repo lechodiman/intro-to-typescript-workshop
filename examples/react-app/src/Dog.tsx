9ad6aa5326f5ac99e63af9ecc8fec4d19b3d50cd
Luis Chodiman <lechodiman@uc.cl>
2020-01-24 12:12:08 -0300

    :fire: remove code to show live demo

--- examples/react-app/src/Dog.tsx
+++ examples/react-app/src/Dog.tsx
@@ -1,23 +1,15 @@
 import React, { Fragment } from 'react';
 
-interface DogProps {
-  dog: {
-    name: string;
-    breed: string;
-  };
-  isAmazing: boolean;
-}
-
-const Dog: React.FC<DogProps> = ({ dog: { breed, name }, isAmazing }) => {
+const Dog: React.FC<DogProps> = () => {
   return (
     <Fragment>
-      <p>Dog name: {name}</p>
+      {/* <p>Dog name: {name}</p>
       <p>Dog breed: {breed}</p>
       {isAmazing ? (
         <p> This dog is amazing</p>
       ) : (
         <p>All dogs are amazing anyways</p>
-      )}
+      )} */}
     </Fragment>
   );
 };
